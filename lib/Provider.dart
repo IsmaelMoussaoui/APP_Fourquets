6c05e7a1d306388b4ba97c090b5b883ff169ce3f
Moussaoui <ismael.moussaoui@epitech.eu>
2020-07-29 12:37:43 +0200

    Refactoring code of forms provider

--- lib/Provider.dart
+++ lib/Provider.dart
@@ -1,7 +1,6 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
-/// This is the stateful widget that the main application instantiates.
 class NewProvider extends StatefulWidget {
   NewProvider({Key key}) : super(key: key);
 
@@ -9,12 +8,10 @@ class NewProvider extends StatefulWidget {
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-class contactProvider
-{
+class contactProvider {
   String namep;
   String phoneNumberp;
   String emailp;
-
   String get name => namep;
   String get phoneNumber => phoneNumberp;
   String get email => emailp;
@@ -34,88 +31,98 @@ class contactProvider
   contactProvider({this.namep, this.emailp, this.phoneNumberp});
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
 class _MyStatefulWidgetState extends State<NewProvider>
 {
   final _formKey = GlobalKey<FormState>();
-  final Contact = new contactProvider();
+  var Contact = new contactProvider();
 
   @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            autofocus: true,
-            decoration: const InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: 'Nom du commercial',
-                labelText: "Nom",
-                contentPadding: EdgeInsets.all(20.0)
+  Widget build(BuildContext context)
+  {
+    String _name;
+    String _numberPhone;
+    String _mail;
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Container(
+              width: 350,
+              padding: EdgeInsets.all(10),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.person),
+                          hintText: 'Entrez le nom du commercial',
+                          labelText: 'Nom'),
+                        keyboardType: TextInputType.name,
+                        validator: (name) {
+                          _name = name;
+                          if (_name.isEmpty)
+                            return 'Remplissez le champ';
+                          return null;
+                        }
+                      ),
+
+                      TextFormField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.phone),
+                          hintText: 'Entrez le numéro de téléphone du commercial',
+                          labelText: 'Numéro'),
+                        keyboardType: TextInputType.phone,
+                        validator: (numberPhone){
+                          _numberPhone = numberPhone;
+                          if (_numberPhone.isEmpty)
+                            return 'Remplissez le champ';
+                          return null;
+                        }
+                      ),
+
+                      TextFormField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.mail),
+                          hintText: "Entrez l'adresse mail du commercial",
+                          labelText: "Email"),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (mail){
+                          _mail = mail;
+                          if (_mail.isEmpty)
+                            return 'Remplissez le champ';
+                          return null;
+                        }
+                      ),
+
+                      new Container(
+                        padding: const EdgeInsets.all(10),
+                        child: RaisedButton(
+                          child: Text('Valider'),
+                          onPressed: (){
+                            if (_formKey.currentState.validate()){
+                              Scaffold.of(context)
+                                .showSnackBar(SnackBar(content: Text(_name + " ajouté aux commerciaux")));
+                              }
+                            },
+                        ),
+                      )
+
+
+                    ]
+                ),
+              ),
             ),
-            keyboardType: TextInputType.name,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Entrez un nom';
-              }
-              Contact.namep = value;
-              return null;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-                icon: Icon(Icons.phone),
-                hintText: "Numéro du commercial",
-                labelText: "Numéro",
-                contentPadding: EdgeInsets.all(20.0)
-            ),
-            keyboardType: TextInputType.phone,
-            validator: (value) {
-              var potentialNumber = int.tryParse(value);
-              if (potentialNumber == null) {
-                return 'Entrez un numéro de téléphone';
-              }
-              Contact.phoneNumberp = value;
-              return null;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.mail),
-              hintText: "Adresse mail du commercial",
-              labelText: "Email",
-              contentPadding: EdgeInsets.all(20.0),
-            ),
-            keyboardType: TextInputType.emailAddress,
-            validator: (value) {
-              if (!EmailValidator.validate(value)) {
-                return 'Entrez une adresse email valide';
-              }
-              Contact.emailp = value;
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 35.0),
-            child: RaisedButton(
-              onPressed: () {
-                final form = _formKey.currentState;
-                if(form.validate()){
-                  form.save();
-                  Contact.save();
-                }
-              },
-              child: Text('Ajouter'),
-            ),
-          ),
-        ],
+          ],
+        ),
+        ),
       ),
     );
-  }
-  _showDialog(BuildContext context) {
-    Scaffold.of(context)
-        .showSnackBar(SnackBar(content: Text('Submitting form')));
+
   }
 }
